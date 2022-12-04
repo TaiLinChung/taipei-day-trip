69ad3ef741cba3535596b3ed0fe382c8128991c3
TaiLinChung <uee632@gmail.com>
2022-12-04 23:24:25 +0800

    add touch

--- static/attraction.js
+++ static/attraction.js
@@ -52,15 +52,17 @@ function getAttractionData(){
     }).then(function(data){
         // console.log(data["data"]["images"][0]);
         imgBackgroundList=data["data"]["images"]
-        // console.log(imgBackgroundList);
+        console.log(imgBackgroundList);
         imgBackground.setAttribute('src',imgBackgroundList[0]);
-        console.log(data["data"]);
+        // console.log(data["data"]);
         attractionName.textContent=data["data"]["name"];
         attractionSort.textContent=(data["data"]["category"]+"at"+data["data"]["mrt"])
         describe.textContent=data["data"]["description"];
         address.textContent=data["data"]["address"];
         transport.textContent=data["data"]["transport"];
         carouselFunction();
+        // apple();
+
     })
 }
 getAttractionData();
@@ -68,8 +70,9 @@ getAttractionData();
 
 
 //carouselfunction
-let carouseBlackPoint="";
+// let carouseBlackPoint="";
 let carousePosition=0;
+let eachCarouselInturnID=0;
 function carouselFunction(){
     carousePosition=0;
     console.log(carousePosition);
@@ -80,10 +83,17 @@ function carouselFunction(){
     for(let i=0;i<imgAmounts;i++){
         let carouselInturn=document.createElement('div');
         carouselInturn.setAttribute('class','carouselInturn');
+        //carouselInturn add id attribute by img position
+        carouselInturn.setAttribute('id',i);
         let carouselInturnshow=document.createElement('div');
         carouselInturnshow.setAttribute('class','carouselInturnshow');
+        //carouselInturnshow add id attribute by img position
+        carouselInturnshow.setAttribute('id',i);
         carouselInturn.appendChild(carouselInturnshow);
         carouselUnderBlock.appendChild(carouselInturn);
+        //set listener to each BlackPointBlock
+        eachCarouselInturnID=i;
+        setListenerforEachCarouselInturn();
     }
     //Default Black Point show the first one
     let carouseBlackPoint=document.querySelectorAll(".carouselInturnshow");
@@ -132,8 +142,61 @@ function setBlackPoint(){
     for(let i=0;i<length;i++){
         carouseBlackPoint[i].style.display="none";
     }
+    // console.log("我在這",carousePosition);
     //  set the position
     carouseBlackPoint[carousePosition].style.display="block";
     imgBackground.setAttribute('src',imgBackgroundList[carousePosition]);
 }
+
+
+
+
+//替每個白球設置監聽事件----------
+function setListenerforEachCarouselInturn(){
+    let CarouselInturnListener=document.getElementById(eachCarouselInturnID);
+    CarouselInturnListener.onclick=function(event){
+        // console.log(event);
+        //All blackPoints display:none
+        //the new position
+        carousePosition=Number(event.target.id);
+        // console.log(typeof(carousePosition));
+        setBlackPoint();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+//-----------------------------------------------
+
+// let positionapple=0;
+// function apple(){
+//     //imgBackgroundList
+//     console.log(imgBackgroundList);
+//     // console.log(imgBackgroundList[0]);
+//     // console.log(imgBackgroundList.length);
+//     // console.log(positionapple);
+    
+//     let left=document.querySelector(".carouselBtnleft");
+//     left.addEventListener("click",function(){
+//         positionapple-=1;
+//         console.log(positionapple);
+//         // console.log(imgBackgroundList[positionapple]);
+
+//     },false)
+
+//     let right=document.querySelector(".carouselBtnright");
+//     right.addEventListener("click",function(){
+//         positionapple+=1;
+//         console.log(positionapple);
+//         // console.log(imgBackgroundList[positionapple]);
+
+//     },false)
+// }
 
