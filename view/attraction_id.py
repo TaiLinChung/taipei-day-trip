5db87570bc8521ac7ab698a50fc1a5fb7cf5e895
TaiLinChung <uee632@gmail.com>
2022-12-05 23:10:26 +0800

    add simple try & except try to fix the web destory suddenly

--- view/attraction_id.py
+++ view/attraction_id.py
@@ -17,37 +17,40 @@ attraction_id_blueprint=Blueprint("attraction_blueprint",__name__)
 
 @attraction_id_blueprint.route("/api/attraction/<attractionId>",methods=["GET"])
 def api_attractionId(attractionId):
-	get_attractionId=attractionId
-	# print(content)
-	mycursor=mydb.cursor()
-	sql="SELECT *FROM datas3 WHERE id=%s"
-	adr=(get_attractionId,)
-	mycursor.execute(sql,adr)
-	myresult=mycursor.fetchall()
-	# print(myresult)
-	if myresult != []:
-		mytitle = mycursor.description
-		column_name =[col[0] for col in mytitle]
-		# print(column_name)
-		# print(myresult[0])
-		data=[]
-		for i in range(0,1):
-			data.append(dict(zip(column_name,list(myresult[i]))))
-		data=data[0]
-		data["images"]=data["images"].split(" ")
-		imgs_notnull=[]
-		for e in data["images"]:
-			if e!="" and ".MP3" not in e and ".FLV" not in e:
-				imgs_notnull.append(e)
-		data["images"]=imgs_notnull
-		print(data)
+	try:
+		get_attractionId=attractionId
+		# print(content)
+		mycursor=mydb.cursor()
+		sql="SELECT *FROM datas3 WHERE id=%s"
+		adr=(get_attractionId,)
+		mycursor.execute(sql,adr)
+		myresult=mycursor.fetchall()
+		# print(myresult)
+		if myresult != []:
+			mytitle = mycursor.description
+			column_name =[col[0] for col in mytitle]
+			# print(column_name)
+			# print(myresult[0])
+			data=[]
+			for i in range(0,1):
+				data.append(dict(zip(column_name,list(myresult[i]))))
+			data=data[0]
+			data["images"]=data["images"].split(" ")
+			imgs_notnull=[]
+			for e in data["images"]:
+				if e!="" and ".MP3" not in e and ".FLV" not in e:
+					imgs_notnull.append(e)
+			data["images"]=imgs_notnull
+			print(data)
 
-		return jsonify({
-				"data": data
-			})
+			return jsonify({
+					"data": data
+				})
 
-	else:
-		return jsonify({
-				"error": True,
-				"message":"景點編號不正確"
-			})
+		else:
+			return jsonify({
+					"error": True,
+					"message":"景點編號不正確"
+				})
+	except:
+		return {"error":True},500
--- view/attractions.py
+++ view/attractions.py
@@ -17,97 +17,94 @@ attractions_blueprint=Blueprint("attractions_blueprint",__name__)
 
 @attractions_blueprint.route("/api/attractions",methods=["GET"])
 def api_attractions():
-	# try:
+	try:
 	
-	page_num=int(request.args.get("page",""))
-	keyword=request.args.get("keyword","")
-	page_maxnum=12
-	##單純頁數
-	if keyword == "":
-		mycursor=mydb.cursor()
-		sql_fetchall="SELECT *from datas3 LIMIT %s,%s"
-		first_num=page_num*0
-		last_num=(page_num+1)*page_maxnum-1
-		adr_fetchall=(page_num*page_maxnum,page_maxnum)
-		mycursor.execute(sql_fetchall,adr_fetchall)
-		myresult_fetchall=mycursor.fetchall()
-		total_amount=len(myresult_fetchall)
-		# print(myresult_fetchall[0])
-		# print(myresult_fetchall)       
+		page_num=int(request.args.get("page",""))
+		keyword=request.args.get("keyword","")
+		page_maxnum=12
+		##單純頁數
+		if keyword == "":
+			mycursor=mydb.cursor()
+			sql_fetchall="SELECT *from datas3 LIMIT %s,%s"
+			first_num=page_num*0
+			last_num=(page_num+1)*page_maxnum-1
+			adr_fetchall=(page_num*page_maxnum,page_maxnum)
+			mycursor.execute(sql_fetchall,adr_fetchall)
+			myresult_fetchall=mycursor.fetchall()
+			total_amount=len(myresult_fetchall)
+			# print(myresult_fetchall[0])
+			# print(myresult_fetchall)       
 
-		for i in range(len(myresult_fetchall)):
-			myresult_fetchall[i]=list(myresult_fetchall[i])
-			# print()
-			# print(myresult_fetchall[i][9])
-			myresult_fetchall[i][9]=myresult_fetchall[i][9].split(" ")
+			for i in range(len(myresult_fetchall)):
+				myresult_fetchall[i]=list(myresult_fetchall[i])
+				# print()
+				# print(myresult_fetchall[i][9])
+				myresult_fetchall[i][9]=myresult_fetchall[i][9].split(" ")
 
-		if total_amount !=0:
-			mytitle = mycursor.description
-			column_name =[col[0] for col in mytitle]
-			data=[]
-			for i in range(total_amount):
-				data.append(dict(zip(column_name,myresult_fetchall[i])))
-			if len(data)==page_maxnum:
-				next_page=page_num+1
+			if total_amount !=0:
+				mytitle = mycursor.description
+				column_name =[col[0] for col in mytitle]
+				data=[]
+				for i in range(total_amount):
+					data.append(dict(zip(column_name,myresult_fetchall[i])))
+				if len(data)==page_maxnum:
+					next_page=page_num+1
+				else:
+					next_page=None
 			else:
-				next_page=None
-		else:
+				return jsonify({                              
+						"error": True,
+						"message": "沒有資料了"	
+					})
+
 			return jsonify({                              
-					"error": True,
-					"message": "沒有資料了"	
-				})
-
-		return jsonify({                              
-					"nextPage": next_page,
-					"data": data	
-				})
+						"nextPage": next_page,
+						"data": data	
+					})
 
 
 
 
-	#模糊&完全
-	else:
-		mycursor=mydb.cursor()
-		sql_keyword="SELECT *FROM datas3 WHERE category=%s or name like concat('%',%s,'%') LIMIT %s,%s"
-		first_num=page_num*0
-		last_num=(page_num+1)*page_maxnum-1
-		adr_keyword=(keyword,keyword,page_num*page_maxnum,page_maxnum)
-		mycursor.execute(sql_keyword,adr_keyword)
-		myresult_keyword=mycursor.fetchall()
-		total_amount=len(myresult_keyword)
+		#模糊&完全
+		else:
+			mycursor=mydb.cursor()
+			sql_keyword="SELECT *FROM datas3 WHERE category=%s or name like concat('%',%s,'%') LIMIT %s,%s"
+			first_num=page_num*0
+			last_num=(page_num+1)*page_maxnum-1
+			adr_keyword=(keyword,keyword,page_num*page_maxnum,page_maxnum)
+			mycursor.execute(sql_keyword,adr_keyword)
+			myresult_keyword=mycursor.fetchall()
+			total_amount=len(myresult_keyword)
 
-		for i in range(len(myresult_keyword)):
-			myresult_keyword[i]=list(myresult_keyword[i])
-			myresult_keyword[i][9]=myresult_keyword[i][9].split(" ")
+			for i in range(len(myresult_keyword)):
+				myresult_keyword[i]=list(myresult_keyword[i])
+				myresult_keyword[i][9]=myresult_keyword[i][9].split(" ")
 
-		if total_amount !=0:
-			mytitle = mycursor.description
-			column_name =[col[0] for col in mytitle]
-		# 	print()
-		# 	print(column_name)
-		# 	print(myresult_keyword)
-			data=[]
-			for i in range(total_amount):
-				data.append(dict(zip(column_name,myresult_keyword[i])))
-			if len(data)==page_maxnum:
-				next_page=page_num+1
+			if total_amount !=0:
+				mytitle = mycursor.description
+				column_name =[col[0] for col in mytitle]
+			# 	print()
+			# 	print(column_name)
+			# 	print(myresult_keyword)
+				data=[]
+				for i in range(total_amount):
+					data.append(dict(zip(column_name,myresult_keyword[i])))
+				if len(data)==page_maxnum:
+					next_page=page_num+1
+				else:
+					next_page=None
 			else:
-				next_page=None
-		else:
-			return jsonify({                              
-					"error": True,
-					"message": "沒有資料了"	
-				})
+				return jsonify({                              
+						"error": True,
+						"message": "沒有資料了"	
+					})
 
-		return jsonify({                              
-					"nextPage": next_page,
-					"data": data	
-				})
-	# except:
-	# 	return jsonify({
-	# 		"error":True,
-	# 		"message":"奇奇妙妙錯誤"
-	# 	})
+			return jsonify({                              
+						"nextPage": next_page,
+						"data": data	
+					})
+	except:
+		return {"error":True},500
 
 	# finally:
 	# 	mydb.close()
