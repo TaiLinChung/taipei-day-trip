793f71a9738c3ef2bd9f1ca9f2cff5309c9b20d6
TaiLinChung <uee632@gmail.com>
2022-12-30 23:35:00 +0800

    use judge token is none to judge not signin status

--- view/user.py
+++ view/user.py
@@ -26,14 +26,14 @@ def api_user():
         password=registerDataFromFrontEnd["password"]
         if register_data_is_empty(name,email,password):
             return jsonify({"error":True,"message":"註冊資料不可為空"})
-        # if not check_email_format(email):
-        #     return jsonify({"error":True,"message":"信箱型態有誤，點此重新註冊"})
+        if not check_email_format(email):
+            return jsonify({"error":True,"message":"信箱型態有誤，點此重新註冊"})
         if register_email_exist(email):
             return jsonify({"error":True,"message":"信箱已被註冊，點此重新註冊"})
         if register(name,email,password):
             return jsonify({"ok":True})
-        # 擴充 增加table寫入token 與 id_people的關聯 用於驗證
-    except:
+    except Exception as e:
+        print("api_user伺服器內部錯誤: ",e)
         return jsonify({"error":True,"message":"500 伺服器內部錯誤"})
 
 
@@ -52,8 +52,8 @@ def api_user_auth():
             password=SigninDataFromFrontEnd["password"]
             if signin_data_is_empty(email,password):
                 return jsonify({"error":True,"message":"登入資料不可為空"})
-            # if not check_email_format(email):
-            #     return jsonify({"error":True,"message":"信箱型態有誤，請確認"})
+            if not check_email_format(email):
+                return jsonify({"error":True,"message":"信箱型態有誤，請確認"})
             if not signin_account_exist(email,password):
                 return jsonify({"error":True,"message":"400 登入失敗"}) #人為問題
             person_information=signin_account_exist(email,password)
@@ -61,54 +61,28 @@ def api_user_auth():
             response=jsonify({"ok":True})
             response.set_cookie("token",token,max_age = 7 * 24 * 60 * 60)
             return response
-        except:
+        except Exception as e:
+            print("api_user_auth伺服器內部錯誤: ",e)
             return jsonify({"error":True,"message":"500 伺服器內部錯誤"})
 
     if request.method=="GET":
         get_token=request.cookies.get("token")
+        if get_token==None:
+            return jsonify({"data":None})
         try:
             decode=jwt_decode(get_token)
-            # 這裡多加一個利用decode 進資料庫check user_id的步驟
             if not check_user_id_in_token_exist(decode):
                 return jsonify({"data":None})
             return jsonify(decode)
-        except:
-            return jsonify({"data":None})
+        except Exception as e:
+            print("api_user_auth伺服器內部錯誤: ",e)
+            return jsonify({"error":True,"message":"500 伺服器內部錯誤"})
 
     if request.method=="DELETE":
         try:
             response=jsonify({"ok":True})
             response.set_cookie("token",'', expires=0)
             return response
-        except:
+        except Exception as e:
+            print("api_user_auth伺服器內部錯誤: ",e)
             return jsonify({"error":True,"message":"500 伺服器內部錯誤"})
-
-
-
-
-
-# import jwt
-# import datetime
-# def jwt_encode(person_information):
-#     idPersion = person_information["id_people"]
-#     name = person_information["name"]
-#     email = person_information["email"]
-#     secret_key = "wehelpJwtKEY@999"
-#     payload={"data":{"id":idPersion,"email":email,"name":name},'iat': datetime.datetime.utcnow(),'exp':(datetime.datetime.utcnow() + datetime.timedelta(days=7))}
-#     token=jwt.encode(payload,secret_key,algorithm='HS256')      #encode-algorithm
-#     return token
-
-# def jwt_decode(get_token):
-#     secret_key = "wehelpJwtKEY@999"
-#     decoded_token=jwt.decode(get_token,secret_key,algorithms='HS256')     #decode-algorithms
-#     return decoded_token
-
-
-
-
-
-    # # 設置 JWT 有效期限為 10 秒
-    # expiration_time = datetime.timedelta(seconds=10)
-    # header = {"alg": "HS256"}
-    # payload = {"id":idPersion,"email":email,"name":name}
-    # token = pyjwt.PyJWT.encode(header, payload, secret_key, expiration_time)
